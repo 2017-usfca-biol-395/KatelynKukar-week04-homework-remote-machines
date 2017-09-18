4840890a690fce1bb03ca3d0d1f7e65cc6645deb
Katelyn Kukar <kbkukar@usfca.edu>
2017-09-18 19:37:55 +0000

    Add second version updates to fasta_QC_checks

--- fasta_QC_checks.sh
+++ fasta_QC_checks.sh
@@ -15,9 +15,12 @@ date
 
 for file in $*.fasta
 do
-	echo "This is a log file named $*.fasta"
-	echo "This file $*.fasta is $(ls -la $*.fasta) large"
-	echo "This file $*.fasta reads in the last three lines $(Head -3 $*.fasta)"
-	echo "This file $*.fasta reads in the last three lines $(Tail -3 $*.fasta)"
+	echo "This is a log file named $file"
+	echo
+	echo "File size:"
+	ls -lah *.fasta |awk '{ print $5}'
+	echo "This file $file reads in the first three lines: $(Head -3 $*.fasta)"
+	Head -3 $file
+	echo "This file $file reads in the last three lines $(Tail -3 $*.fasta)"
 	echo "This file $*.fasta  has $(grep -c ">" $file) number or sequences"
 	echo 
